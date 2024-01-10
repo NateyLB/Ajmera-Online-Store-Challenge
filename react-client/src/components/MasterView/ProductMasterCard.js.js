e15044b31b24f4c41a18f82e4e386eef545c001a
Nathan <nateyl.b@gmail.com>
2024-01-09 22:19:20 -0800

    mobile master view products are now clickable and actionable

--- react-client/src/components/MasterView/ProductMasterCard.js.js
+++ react-client/src/components/MasterView/ProductMasterCard.js.js
@@ -1,10 +1,11 @@
-import { Card, CardContent, CardHeader, CardMedia, Typography, Rating, Container, Chip, Grid } from "@mui/material";
+import { Card, CardContent, CardHeader, CardMedia, Typography, Rating, Container, Chip, Grid, CardActionArea } from "@mui/material";
 import React from "react";
 
 const ProductMasterCard = (props) =>{
     return(
         <Grid item xs={6} >
         <Card sx={{height:1}}>
+            <CardActionArea href={`/product/${props.product.id}`}>
             <CardHeader 
                 title={props.product.title} 
                 sx = {{height:1/4}}
@@ -16,6 +17,10 @@ const ProductMasterCard = (props) =>{
                 alt={props.product.title}
                 sx = {{height:1/2}}
             />
+            <Container sx={{justifyContent:"flexStart", display:"flex", margin:"none", paddingRight: 10/100}}>
+                <Rating name="half-rating" defaultValue={props.product.rating.rate} precision={0.1} size="medium" />
+                <Chip label={props.product.rating.count} variant="outlined" size="small"/>
+            </Container>
             <CardContent >
                 <Typography>
                     ${props.product.price}
@@ -24,6 +29,7 @@ const ProductMasterCard = (props) =>{
                     {props.product.category}
                 </Typography>
             </CardContent>
+            </CardActionArea>
         </Card>
         </Grid>
     )
