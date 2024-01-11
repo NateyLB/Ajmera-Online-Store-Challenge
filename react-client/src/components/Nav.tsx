7931d6b65e6112eb9cd49aae29790238314298cc
Nathan <nateyl.b@gmail.com>
2024-01-10 22:50:10 -0800

    updated Nav to be more responsive

--- react-client/src/components/Nav.tsx
+++ react-client/src/components/Nav.tsx
@@ -9,11 +9,11 @@ const Nav = () => {
     return(
         <AppBar position="static" >
         <Toolbar id="tool" sx={{padding:"0!important", display: "flex", flexDirection: "row", width: "100%", alignContent: "center", justifyContent: "center" }}>
-            <MenuIcon sx={{marginRight: "20%"}} fontSize="medium" color="black"/>
+            <MenuIcon sx={{marginRight:{xs:"20%", sm:"35%", md:"40%", lg:"42%", xl:"44%"}}} fontSize="medium" />
             <Link variant="h6" color="inherit" underline="none" href="/">
                 eCommerce
             </Link>
-            <SearchIcon sx={{marginLeft:"20%"}} fontSize="medium"/>
+            <SearchIcon sx={{marginLeft:{xs:"20%", sm:"35%", md:"40%", lg:"42%", xl:"44%"}}} fontSize="medium"/>
             <ShoppingCartOutlinedIcon fontSize="medium" />
         </Toolbar>
         </AppBar>
